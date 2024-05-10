2a23c159e4a150498f0da6388769219d62d6aedb
Lubomir Karnatov <lubomir.karnatov@gmail.com>
2024-05-11 01:37:38 +0300

    initial commit

---
+++ car-passion-network-api/src/main/java/com/carpassionnetwork/CarPassionNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.carpassionnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPassionNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPassionNetworkApiApplication.class, args);
+	}
+
+}
---
+++ car-passion-network-api/src/test/java/com/carpassionnetwork/CarPassionNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpassionnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPassionNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
