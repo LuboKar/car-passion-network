f5884de7102bde62180cb408c740d603387a8c2f
Lubomir Karnatov <lubomir.karnatov@gmail.com>
2024-09-03 19:26:59 +0300

    UI: added isExpired to token service

--- car-passion-network-ui/src/component/Authentication/Authentication.js
+++ car-passion-network-ui/src/component/Authentication/Authentication.js
@@ -1,11 +1,5 @@
-import { jwtDecode } from "jwt-decode";
+import { isExpired } from "../service/TokenService";
 
 export const isAuthenticated = () => {
-  const token = localStorage.getItem("jwtToken");
-  if (token) {
-    const decodedToken = jwtDecode(token);
-    const currentTime = Date.now() / 1000;
-    return decodedToken.exp > currentTime;
-  }
-  return false;
+  return isExpired();
 };
--- car-passion-network-ui/src/component/service/TokenService.js
+++ car-passion-network-ui/src/component/service/TokenService.js
@@ -1,14 +1,31 @@
 import { jwtDecode } from "jwt-decode";
 
+const getToken = () => {
+  return localStorage.getItem("jwtToken");
+};
+
 const getId = () => {
-  const token = localStorage.getItem("jwtToken");
+  const token = getToken();
 
   if (token) {
     const decodedToken = jwtDecode(token);
     const id = decodedToken.userId;
     return id;
   }
+
   return null;
 };
 
-export { getId };
+const isExpired = () => {
+  const token = getToken();
+
+  if (token) {
+    const decodedToken = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    return decodedToken.exp > currentTime;
+  }
+
+  return false;
+};
+
+export { getId, isExpired, getToken };
