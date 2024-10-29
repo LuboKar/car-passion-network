1896a6c42eec8a03d332b59b70df059db7756c98
Lubomir Karnatov <lubomir.karnatov@gmail.com>
2024-10-29 16:38:45 +0200

    API: post mapper refactor

--- car-passion-network-api/src/main/java/com/carpassionnetwork/mapper/PostMapper.java
+++ car-passion-network-api/src/main/java/com/carpassionnetwork/mapper/PostMapper.java
@@ -4,12 +4,12 @@ import com.carpassionnetwork.dto.request.PostCreateRequestDto;
 import com.carpassionnetwork.dto.response.CommentResponseDto;
 import com.carpassionnetwork.dto.response.PostResponseDto;
 import com.carpassionnetwork.model.Post;
-import com.carpassionnetwork.model.User;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -41,16 +41,9 @@ public class PostMapper {
   }
 
   private boolean isCurrentUserLiked(Post post) {
-    String currentUserEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+    UserDetails currentUser =
+        (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-    if (post.getLikes() != null) {
-      for (User user : post.getLikes()) {
-        if (user.getEmail().equals(currentUserEmail)) {
-          return true;
-        }
-      }
-    }
-
-    return false;
+    return post.getLikes().contains(currentUser);
   }
 }
