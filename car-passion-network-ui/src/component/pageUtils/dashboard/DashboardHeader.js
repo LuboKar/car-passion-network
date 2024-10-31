e314b487cd72af9ead12b5e13c25d344f5bea69b
Lubomir Karnatov <lubomir.karnatov@gmail.com>
2024-10-31 20:32:30 +0200

    UI: dashboard header refactor

--- car-passion-network-ui/src/component/pageUtils/dashboard/DashboardHeader.js
+++ car-passion-network-ui/src/component/pageUtils/dashboard/DashboardHeader.js
@@ -4,12 +4,12 @@ import "./DashboardHeader.css";
 export default function DashboardHeader() {
   return (
     <div className="dashboard-header-container">
-      <label className="dashboard-header-text">
+      <label className="dashboard-header-label">
         Welcome to the Car Passion Network.
       </label>
-      <label className="dashboard-header-text">This is your dashboard.</label>
-      <label className="dashboard-header-text">There are no posts yet.</label>
-      <label className="dashboard-header-text">
+      <label className="dashboard-header-label">This is your dashboard.</label>
+      <label className="dashboard-header-label">There are no posts yet.</label>
+      <label className="dashboard-header-label">
         Be the first to write a post!
       </label>
     </div>
