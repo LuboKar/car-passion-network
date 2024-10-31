2ca42005cde1195c3f77446c947157959b7205d5
Lubomir Karnatov <lubomir.karnatov@gmail.com>
2024-10-31 20:56:52 +0200

    UI: home page header refactor

--- car-passion-network-ui/src/component/pageUtils/home/HomePageHeader.js
+++ car-passion-network-ui/src/component/pageUtils/home/HomePageHeader.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "../home/HomePageHeader.css";
 
-export default function Header() {
+export default function HomePageHeader() {
   return (
-    <div className="page_content">
+    <div className="home-page-header-container">
       <h1 className="homepage-header">Car Passion Network</h1>
       <p className="homepage-text">Discuss anything related to cars</p>
     </div>
