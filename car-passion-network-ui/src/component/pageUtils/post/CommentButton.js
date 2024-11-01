40e1ef75f0857d53f57aff501b5ff3974cedfa7e
Lubomir Karnatov <lubomir.karnatov@gmail.com>
2024-11-01 15:48:27 +0200

    UI: comment button refactor

--- car-passion-network-ui/src/component/pageUtils/post/CommentButton.js
+++ car-passion-network-ui/src/component/pageUtils/post/CommentButton.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./CommentButton.css";
 import commentIcon from "../../../images/comment-icon.png";
 
-export default function WriteComment({ toggleCommentsFunction }) {
+export default function CommentButton({ toggleCommentsFunction }) {
   return (
-    <div className="comment-button-div" onClick={toggleCommentsFunction}>
-      <img src={commentIcon} alt="icon" className="comment-icon" />
-      <label className="comment-button">Comment</label>
+    <div className="comment-button-container" onClick={toggleCommentsFunction}>
+      <img src={commentIcon} alt="icon" className="comment-button-icon" />
+
+      <label className="comment-button-label">Comment</label>
     </div>
   );
 }
