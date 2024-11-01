53e21eabfb7f10d0d1c0acf5cf8062db05152d7f
Lubomir Karnatov <lubomir.karnatov@gmail.com>
2024-11-01 17:24:53 +0200

    UI: post likes dropdown refactor

--- car-passion-network-ui/src/component/pageUtils/post/PostLikesDropdown.js
+++ car-passion-network-ui/src/component/pageUtils/post/PostLikesDropdown.js
@@ -1,39 +1,29 @@
 import React from "react";
 import useNavigation from "../../service/NavigateService";
-import pic from "../../../images/profile-pic.jpg";
 import "./PostLikesDropdown.css";
+import User from "../user/User";
 
 export default function PostLikesDropdown({ post, clickedLikes }) {
   const { navigateToProfile } = useNavigation();
+
   return (
-    <div className="drowdown-container">
-      <div className="dropdown-menu">
-        <label className="close-likes" onClick={() => clickedLikes(0)}>
+    <div className="post-likes-drowdown-container">
+      <div className="post-likes-dropdown-menu">
+        <label
+          className="post-likes-close-menu"
+          onClick={() => clickedLikes(0)}
+        >
           X
         </label>
       </div>
-      <div className="likes-dropdown">
+
+      <div className="post-likes-likes-dropdown">
         {post.likes.map((user, index) => (
-          <div
-            key={index}
-            className="liked-user-container"
-            onClick={() => navigateToProfile(user.id)}
-          >
-            <div className="post-user-like-pic-container">
-              <img
-                src={
-                  user.profilePicture
-                    ? `http://localhost:8080/${user.profilePicture}`
-                    : pic
-                }
-                alt="user-pic"
-                className="liked-user-pic"
-              />
-            </div>
-            <label key={index} className="liked-user-name">
-              {user.firstName} {user.lastName}
-            </label>
-          </div>
+          <User
+            user={user}
+            index={index}
+            navigateToProfile={() => navigateToProfile(user.id)}
+          />
         ))}
       </div>
     </div>
