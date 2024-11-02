9468ecb23be13015f05d491b25f2d845ed37ab25
Lubomir Karnatov <lubomir.karnatov@gmail.com>
2024-11-02 18:50:35 +0200

    UI: user refactor

--- car-passion-network-ui/src/component/pageUtils/user/User.js
+++ car-passion-network-ui/src/component/pageUtils/user/User.js
@@ -1,26 +1,15 @@
 import React from "react";
 import "./User.css";
-import pic from "../../../images/profile-pic.jpg";
+import ProfilePicture from "./ProfilePicture";
 
 export default function User({ user, index, navigateToProfile }) {
   return (
-    <div
-      key={index}
-      className="comment-likes-container"
-      onClick={navigateToProfile}
-    >
-      <div className="comment-user-like-pic-container">
-        <img
-          src={
-            user.profilePicture
-              ? `http://localhost:8080/${user.profilePicture}`
-              : pic
-          }
-          alt="user-pic"
-          className="user-like-pic"
-        />
+    <div key={index} className="user-container" onClick={navigateToProfile}>
+      <div className="user-picture-container">
+        <ProfilePicture profilePicture={user.profilePicture} />
       </div>
-      <label key={index} className="user-like-name">
+
+      <label key={index} className="user-name">
         {user.firstName} {user.lastName}
       </label>
     </div>
