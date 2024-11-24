6840eb196b23a1bfc685721e1ecf90ec6014c20b
Lubomir Karnatov <lubomir.karnatov@gmail.com>
2024-11-24 17:23:41 +0200

    API: fixed exception message

--- car-passion-network-api/src/main/java/com/carpassionnetwork/exception/AlreadyUsedGroupNameException.java
+++ car-passion-network-api/src/main/java/com/carpassionnetwork/exception/AlreadyUsedGroupNameException.java
@@ -2,6 +2,6 @@ package com.carpassionnetwork.exception;
 
 public class AlreadyUsedGroupNameException extends RuntimeException {
   public AlreadyUsedGroupNameException() {
-    super("This group name already used!");
+    super("This group name is already used!");
   }
 }
