8177866a9e1e2e02f8c2d16fa652ce0eb37566bf
Lubomir Karnatov <lubomir.karnatov@gmail.com>
2024-12-02 18:58:27 +0200

    UI: added feed and groups button to dashboard page

--- car-passion-network-ui/src/component/page/DashboardPage.js
+++ car-passion-network-ui/src/component/page/DashboardPage.js
@@ -6,9 +6,17 @@ import "./DashboardPage.css";
 import DashboardHeader from "../pageUtils/dashboard/DashboardHeader.js";
 import { getId } from "../service/TokenService.js";
 import { PostsContext } from "../context/PostsProvider.js";
+import VerticalNavbar from "../pageUtils/navbar/VerticalNavbar.js";
+import feedIcon from "../../images/feed.png";
+import groupIcon from "../../images/groups.png";
+import { getGroupsByAdmin } from "../service/GroupService.js";
+import Groups from "../group/Groups.js";
 
 export default function DashboardPage() {
   const [loadingPosts, setLoadingPosts] = useState(true);
+  const [viewFeed, setViewFeed] = useState(true);
+  const [viewGroups, setViewGroups] = useState(false);
+  const [userAdminGroups, setUserAdminGroups] = useState([]);
   const currentUserId = getId();
 
   const { posts, setPosts } = useContext(PostsContext);
@@ -25,22 +33,76 @@ export default function DashboardPage() {
     setLoadingPosts(false);
   };
 
+  const fetchAdminGroups = async () => {
+    const response = await getGroupsByAdmin(currentUserId);
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const groupData = await response.json();
+    setUserAdminGroups(groupData);
+  };
+
   useEffect(() => {
     fetchPosts();
+    fetchAdminGroups();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const disbleAllButtons = () => {
+    setViewFeed(false);
+    setViewGroups(false);
+  };
+
+  const toggleFeed = () => {
+    disbleAllButtons();
+    setViewFeed(true);
+  };
+
+  const toggleGroups = () => {
+    disbleAllButtons();
+    setViewGroups(true);
+  };
+
+  const topButtons = [
+    {
+      label: "Feed",
+      icon: feedIcon,
+      onClick: toggleFeed,
+      isVisible: viewFeed,
+    },
+    {
+      label: "Groups",
+      icon: groupIcon,
+      onClick: toggleGroups,
+      isVisible: viewGroups,
+    },
+  ];
 
   return (
     <div className="dashboard-container">
       <Navbar />
 
-      {!loadingPosts && (
+      <VerticalNavbar topButtons={topButtons} />
+
+      {!loadingPosts && viewFeed && (
         <div className="dashboard-posts-container">
           <Posts ownerId={currentUserId} />
         </div>
       )}
 
       {posts.length < 1 && !loadingPosts && <DashboardHeader />}
+
+      {viewGroups && (
+        <div className="dashboard-groups-container">
+          <Groups
+            userId={currentUserId}
+            userAdminGroups={userAdminGroups}
+            setUserAdminGroups={setUserAdminGroups}
+          />
+        </div>
+      )}
     </div>
   );
 }
